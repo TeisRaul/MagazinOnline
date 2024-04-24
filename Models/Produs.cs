e0a9613ebeaa88889457a18702fa6d4c5a0c93c6
TeisRaul <antonio.teis@student.upt.ro>
2024-04-24 15:14:25 +0300

    Clasa Utilizator v1

--- Models/Produs.cs
+++ Models/Produs.cs
@@ -1,6 +1,19 @@
-﻿namespace Magazin_Online.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Magazin_Online.Models
 {
     public class Produs
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Denumire {  get; set; }
+        //public CategorieProdus Categorie { get; set; }
+        public float Pret { get; set; }
+        public int NumarBucati { get; set; }
+        public string Descriere { get; set; }
+        public string Imagine { get; set; }
+        //public Orase Orase { get; set; }
+
     }
 }
---
+++ Models/Utilizator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace Magazin_Online.Models
+{
+    public class Utilizator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] public string Nume { get; set; }
+        [Required] public string Prenume { get; set; }
+        [Required] public string Email { get; set; }
+        [Required] public string Parola { get; set; }
+        [Required] public string Adresa { get; set; }
+        //[Required] public Orase Oras { get; set; }
+        [Required] public string Telefon { get; set; }
+    }
+}
