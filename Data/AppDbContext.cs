c6e50f854c76f1a1037177d5dbf5c9101fdb056b
TeisRaul <antonio.teis@student.upt.ro>
2024-04-24 16:08:12 +0300

    AppDbContext.cs v1

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -4,5 +4,7 @@ namespace Magazin_Online.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+         
     }
 }
