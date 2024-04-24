ac021383af3caca555ba3177af1359f4ef51aede
TeisRaul <antonio.teis@student.upt.ro>
2024-04-24 20:07:30 +0300

    AppDbInitializer.cs added

---
+++ Data/AppDbInitializer.cs
@@ -0,0 +1,71 @@
+﻿using Magazin_Online.Data.Enums;
+using Magazin_Online.Data;
+using Magazin_Online.Models;
+using MagazinOnline.Data;
+using MagazinOnline.Data.Enums;
+using MagazinOnline.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MagazinOnline.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                if (!context.Admini.Any())
+                {
+                    context.Admini.AddRange(new Admin
+                    {
+                        Username = "admin",
+                        Password = "admin"
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Utilizatori.Any())
+                {
+                    context.Utilizatori.AddRange(new Utilizator
+                    {
+                        Nume = "Teis",
+                        Prenume = "Raul",
+                        Email = "teisraul@yahoo.co.uk",
+                        Parola = "parola",
+                        Adresa = "Str. Mihai Eminescu, nr. 1",
+                        Oras = Orase.Oradea,
+                        Telefon = "0723456789",
+                        AdminId = 1
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Produse.Any())
+                {
+                    context.Produse.AddRange(new List<Produs>
+                    {
+                        new Produs
+                        {
+                            Denumire = "Laptop",
+                            Categorie = CategorieProdus.Electronice_si_electrocasnice,
+                            Pret = 3000,
+                            NumarBucati = 3,
+                            Imagine = "laptop.jpg",
+                            Descriere = "Laptop performant",
+                            Orase = Orase.Oradea,
+                            UtilizatorId = 1
+                        }
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/Magazin-Online.AssemblyInfo.cs
+++ obj/Debug/net8.0/Magazin-Online.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magazin-Online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c58ac4a3b7c32c08497556f1a9ff8382a826bf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1290b5bf5f595e0c018874f219e27120fa6ff589")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magazin-Online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magazin-Online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
